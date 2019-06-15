84768c1dc253e75b9bbce6304a5a1abf21d7ab91
Vineet Prasad Verma <vineetpd1996@gmail.com>
2019-06-16 00:11:07 +0530

    Logged in users can chat with each other

--- Registration-And-Login/app.py
+++ Registration-And-Login/app.py
@@ -7,7 +7,11 @@ from flask_socketio import SocketIO, send
 engine = create_engine("mysql+pymysql://root:root@localhost/register")
 db = scoped_session(sessionmaker(bind=engine))
 app = Flask('__name__')
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
 
+# logged_in_users = set() # TODO: Make it work with set
+logged_in_users = []
 
 @app.route("/")
 def home():
@@ -52,8 +56,14 @@ def login():
             for password_data in passworddata:
                 if sha256_crypt.verify(password, password_data):
                     session["log"] = True
+                    user_name = usernamedata[0]
+                    session["user_name"] = user_name
                     flash("You are now logged in", "success")
-                    return redirect(url_for('photo'))
+                    message = username
+                    #logged_in_users.add(user_name)
+                    logged_in_users.append(user_name)
+                    print(logged_in_users)
+                    return redirect(url_for('photo', messages=message))
                 else:
                     flash("Incorrect password", "danger")
                     return render_template("login.html")
@@ -62,16 +72,33 @@ def login():
 #photo
 @app.route("/photo")
 def photo():
-    return render_template("photo.html")
+    messages = request.args['messages']
+    return render_template("photo.html", messages=messages)
 
 #logout
 @app.route("/logout")
 def logout():
+    user_name = session.get("user_name")
+    if user_name in logged_in_users:
+        logged_in_users.remove(user_name)
     session.clear()
     flash("You are now logged out !!", "success")
+    print(logged_in_users)
     return redirect(url_for('login'))
+
+
+@socketio.on('message')
+def handle_message(msg):
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
+
+
+@socketio.on('connect')
+def handle_connect():
+    send(logged_in_users, broadcast=True)
 
 
 if __name__ == "__main__":
     app.secret_key = "captainjacksparrow"
+    socketio.run(app)
     app.run(debug=True)
